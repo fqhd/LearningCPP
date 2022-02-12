030f26d9f366344ee11b5303f8a162658fa9d0bc
fqhd <fahd31256@gmail.com>
2022-02-12 11:21:05 +0100

    operator keywoard with functions (* + / - etc...)

--- main.cpp
+++ main.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
-#include <thread> // Include this for thread support(no need for -pthread in the build instruction)
-#include <mutex>
 
-std::mutex mutex; // Mutex must be in shared memory between both threads so both threads can access it(Can also be passed to the thread as a pointer)
+struct Vector2 {
+	float x, y;
 
-void do_work(){
-	mutex.lock(); // If mutex is unlocked, lock the mutex. If mutex is locked, wait for mutex to unlock, then lock the mutex
-	std::this_thread::sleep_for(std::chrono::seconds(1)); // Sleeps on current thread for 1 second
-	mutex.unlock();
-}
+	Vector2(float x, float y){
+		this->x = x;
+		this->y = y;
+		std::cout << "hi" << std::endl;
+	}
+	Vector2 operator+(const Vector2& other) const { // Use const in member functions that don't change the data of the class
+		return Vector2(this->x + other.x, this->y + other.y);
+	}
+	Vector2 operator*(const Vector2& other) const {
+		return Vector2(this->x * other.x, this->y * other.y);
+	}
+	Vector2 operator*(const float scalar) const {
+		return Vector2(this->x * scalar, this->y * scalar);
+	}
+};
+
 
 int main(){
-	auto start = std::chrono::high_resolution_clock::now();
-	std::thread worker(do_work); // Automatically starts the first thread
-	std::thread worker2(do_work); // Automatically starts the second thread
+	// Creates two vectors
+	Vector2 a(5, 5);
+	Vector2 b(6, 8);
 
-	worker.join(); // Waits for first thread to finish
-	worker2.join(); // Waits for second thread to finish
-	auto end = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<float> time = end - start; // Measures the time it took
-	std::cout << "It took " << time.count() << " seconds" << std::endl; // Prints the time it took to run both threads
-
+	// Uses the member operator functions to initialize a new vector
+	Vector2 c = a + b * a * 2.0f;
+	
 	return 0;
 }
