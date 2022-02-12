18fdc7595a5a40abf8efb48060e4321250ec36be
fqhd <fahd31256@gmail.com>
2022-02-12 10:00:24 +0100

    multithreading with mutexes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <thread> // Include this for thread support(no need for -pthread in the build instruction)
+#include <mutex>
+
+std::mutex mutex; // Mutex must be in shared memory between both threads so both threads can access it(Can also be passed to the thread as a pointer)
 
 void do_work(){
+	mutex.lock(); // If mutex is unlocked, lock the mutex. If mutex is locked, wait for mutex to unlock, then lock the mutex
 	std::this_thread::sleep_for(std::chrono::seconds(1)); // Sleeps on current thread for 1 second
+	mutex.unlock();
 }
 
 int main(){
